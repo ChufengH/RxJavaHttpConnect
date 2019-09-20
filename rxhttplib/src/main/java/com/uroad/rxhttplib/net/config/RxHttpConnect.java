62234d6b167195f1adeaef5d64d61f96c7ef4b4a
chufeng <392044178@qq.com>
2019-09-20 16:05:58 +0800

    优化修改

--- rxhttplib/src/main/java/com/uroad/rxhttplib/net/config/RxHttpConnect.java
+++ rxhttplib/src/main/java/com/uroad/rxhttplib/net/config/RxHttpConnect.java
@@ -3,11 +3,11 @@ package com.uroad.rxhttplib.net.config;
 import android.app.Application;
 import com.uroad.rxhttplib.net.until.LogUtil;
 
-public class Config {
+public class RxHttpConnect {
     public static final String base_url = "https://www.baidu.com/";
     public static Application application = null;
-    public static String key = "status";
-    public static String code = "ok";
+    public static String key = "";
+    public static String code = "";
 
     public static void init(String keys, String s, Boolean isDebug) {
         key = keys;
--- rxhttplib/src/main/java/com/uroad/rxhttplib/net/rx/AbstractObserver.java
+++ rxhttplib/src/main/java/com/uroad/rxhttplib/net/rx/AbstractObserver.java
@@ -2,9 +2,8 @@ package com.uroad.rxhttplib.net.rx;
 
 
 import com.alibaba.fastjson.JSONObject;
-import com.blankj.utilcode.util.JsonUtils;
-import com.google.gson.Gson;
-import com.uroad.rxhttplib.net.config.Config;
+
+import com.uroad.rxhttplib.net.config.RxHttpConnect;
 import com.uroad.rxhttplib.net.until.FastJsonUtils;
 import com.uroad.rxhttplib.net.until.JSONUtils;
 import com.uroad.rxhttplib.net.until.LogUtil;
@@ -34,10 +33,10 @@ public abstract class AbstractObserver<T> implements Observer<String> {
     public void onNext(String result) {
         JSONObject jsonObject = JSONObject.parseObject(result);
         LogUtil.i("result", "result = " + result);
-        if (Config.key.equals("") || Config.code.equals("")) { //等于空符串
+        if (RxHttpConnect.key.equals("") || RxHttpConnect.code.equals("")) { //等于空符串
             handleJson(result);
         } else {
-            if (FastJsonUtils.getJsonStatuByStatus(jsonObject, Config.key, Config.code)) { //判断服务器返回数据是否正常
+            if (FastJsonUtils.getJsonStatuByStatus(jsonObject, RxHttpConnect.key, RxHttpConnect.code)) { //判断服务器返回数据是否正常
                 handleJson(result);
             } else {
                 if (FastJsonUtils.getMsg(jsonObject) != null) {
@@ -61,6 +60,7 @@ public abstract class AbstractObserver<T> implements Observer<String> {
                 //调用抽象方法onSuccess
                 onSuccess(t);
             } catch (Exception e) {
+                LogUtil.i("result", "Json解析错误信息：" + e.getMessage());
                 onFailure("数据解析错误");
             }
         } else {
