eb7d3591b690f95d10f55e310ec8a390c2278424
chufeng <392044178@qq.com>
2019-11-22 10:05:46 +0800

    增加了上传文件流的功能

--- app/src/main/java/com/example/rxjavahttpconnect/MainActivity.kt
+++ app/src/main/java/com/example/rxjavahttpconnect/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.rxjavahttpconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.uroad.rxhttplib.net.config.RxHttpConnect
-import com.uroad.rxhttplib.net.rx.RxRestClient
 
 class MainActivity : AppCompatActivity() {
 
